47f2c8037ff55caeafcc7278c7ec5b7feaa62f61
nizaar203040100 <nizaar.203040100.mail.unpas.ac.id>
2022-09-30 15:07:08 +0700

    Mengubah Judul Halaman menggunakan H1 dan mengubah title

--- pw/index.php
+++ pw/index.php
@@ -24,12 +24,12 @@ $buku = $rows;
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Buku</title>
+  <title>Nizaar's book</title>
 </head>
 
 <body>
 
-  <h1>Daftar Buku</h1>
+  <h1>List Book</h1>
 
   <table border="1" cellpading="10" cellspacing="0">
     <tr>
