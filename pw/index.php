32ca0af671229bd45bbd153a122742fe3648d42d
nizaar203040100 <nizaar.203040100.mail.unpas.ac.id>
2022-09-30 14:54:00 +0700

    :prakweb_b_203040100

---
+++ pw/index.php
@@ -0,0 +1,56 @@
+<?php
+// koneksi ke database
+$conn = mysqli_connect("localhost", "root", "", "prakweb_b_203040100");
+
+// ambil dari tabel film / query
+$result = mysqli_query($conn, "SELECT * FROM buku");
+
+// ubah data ke dalam array
+// $row = mysqli_fetch_row($result); // array numerik
+// $row = mysqli_fetch_assoc($result); // array associative
+// $row = mysqli_fetch_array($result); // keduanya
+$rows = [];
+while ($row = mysqli_fetch_assoc($result)) {
+  $rows[] = $row;
+}
+// tampung ke variabel buku
+$buku = $rows;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Buku</title>
+</head>
+
+<body>
+
+  <h1>Daftar Buku</h1>
+
+  <table border="1" cellpading="10" cellspacing="0">
+    <tr>
+      <th>No</th>
+      <th>Judul</th>
+      <th>Penulis</th>
+      <th>Tahun Terbit</th>
+      <th>Gambar</th>
+    </tr>
+    <?php $i = 1; ?>
+    <?php foreach ($buku as $row) : ?>
+      <tr>
+        <td><?= $i++; ?></td>
+        <td><?= $row["judul_buku"]; ?> </td>
+        <td><?= $row["penulis"]; ?></td>
+        <td><?= $row["tahun"]; ?> </td>
+        <td><img src="img/<?= $row["gambar"]; ?>" alt="" width="100"></td>
+        </tr>
+    <?php endforeach; ?>
+  </table>
+
+</body>
+
+</html>
