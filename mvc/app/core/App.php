39c502b60b5b9c3efd210c171c8c9f2c06861c64
nizaar203040100 <nizaar.203040100.mail.unpas.ac.id>
2022-10-14 18:44:43 +0700

    menyelesaikan routing dan controller

--- mvc/app/core/App.php
+++ mvc/app/core/App.php
@@ -1,9 +1,53 @@
 <?php
 
 class App {
+    protected $controller = 'home';
+    protected $method = 'index';
+    protected $params = [];
     public function __construct() {
         echo "Ok";
+        $url = $this->parseURL();
+        //controller
+        if($url == NULL)
+               {
+			$url = [$this->controller];
+		}
+        if(file_exists('../app/controller/' . $url[0] . '.php')) {
+            $this->controller =$url[0];
+            unset($url[0]);
+        }
+
+        require_once '../app/controller/' . $this->controller . '.php';
+        $this->controller = new $this->controller;
+
+
+
+        //method
+        if(isset($url[1])) {
+            if(method_exists($this->controller, $url[1])) {
+                $this->method = $url[1];
+                unset($url[1]);
+            }
+        }
+
+        //params
+        if(!empty($url)) {
+            $this->params = array_values($url);
+
+        }
+
+        //jalankan controller dan method serta kirimkan params jika ada
+        call_user_func_array([$this->controller,$this->method,], $this->params);
+    }
+
+    public function parseURL() {
+        if(isset($_GET['url'])) {
+            $url = rtrim($_GET['url'], '/');
+            $url =filter_var($url, FILTER_SANITIZE_URL);
+            $url =explode('/', $url);
+            return $url;
+        }
+
+
     }
 }
-
-?>
